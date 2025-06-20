b0a57212c65d59b9854008e5a8b9e1290772d548
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:51:52 +0000

    [automatic commit] 2025-06-20 11:51:51 UTC 1.121.164.79

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -175,6 +175,7 @@ function downvote(index) {
 
 
 function login(){
+    //changed 
     let user = {
         username: document.getElementById('username').value,
         password: document.getElementById('password').value
