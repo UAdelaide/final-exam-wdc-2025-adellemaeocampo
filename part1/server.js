5944d69af3027bbfde4bbc3bd9df64ea9c55c175
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 04:05:03 +0000

    [automatic commit] 2025-06-20 04:05:03 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -39,7 +39,7 @@ app.get('/api/dogs', async (req,res) => {
   }
 });
 
-app.get('')
+app.get('/api/walkrequests/open', async())
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
