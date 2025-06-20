14fc3b7113c15875d38e5da3cdea5259ac0d2dce
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 01:56:50 +0000

    [automatic commit] 2025-06-20 01:56:50 UTC 1.121.164.79

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -72,4 +72,4 @@ INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, sta
 VALUES ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
        ((SELECT dog_id FROM Dogs WHERE name = 'Mochi'), '2025-06-10 10:30:00', 25, 'Glenelg', 'cancelled'),
-       ((SELECT dog_id FROM Dogs WHERE name = 'Astro'), '2025-06-10 10:30:00', 25, 'Glenelg', 'cancelled'),
+       ((SELECT dog_id FROM Dogs WHERE name = 'Astro'), '2025-06-20 11:30:00', 60, 'Glenelg', 'cancelled'),
