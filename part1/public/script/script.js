947466dbc126c1c0e574b727d17dd0369118d6ab
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 07:03:25 +0000

    [automatic commit] 2025-06-20 07:03:25 UTC 1.121.164.79

--- part1/public/script/script.js
+++ part1/public/script/script.js
@@ -13,8 +13,8 @@ Vue.createApp ({
       fetch('https://dog.ceo/api/breeds/image/random')
         .then(res=>res.json())
         .then(data=> {
-
+          this.dogImage = data.message;
         });
     }
   }
-}).mount('#dogApp')
+}).mount('#dogApp');
