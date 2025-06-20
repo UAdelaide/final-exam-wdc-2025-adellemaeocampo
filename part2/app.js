11764e6c32245781d124fd1fcda149b1d08f2739
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:31:58 +0000

    [automatic commit] 2025-06-20 11:31:58 UTC 1.121.164.79

--- part2/app.js
+++ part2/app.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-
+app.use(session({
+  
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
