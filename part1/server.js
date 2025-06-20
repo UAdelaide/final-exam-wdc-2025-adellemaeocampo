0609012d51d1457e7819ad8c284c5207e0c08627
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 04:08:55 +0000

    [automatic commit] 2025-06-20 04:08:54 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -41,7 +41,7 @@ app.get('/api/dogs', async (req,res) => {
 
 app.get('/api/walkrequests/open', async(req,res) => {
   try{
-    const[openRequests] = await db.query ('SELECT * FROM WalkRequests ')
+    const[openRequests] = await db.query ('SELECT * FROM WalkRequests WHERE status = 'open'')
   }
 });
 
