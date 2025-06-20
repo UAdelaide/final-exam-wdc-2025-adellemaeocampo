1a1b653c124089d60cf69888d2d5917ba0b96c19
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 01:39:05 +0000

    [automatic commit] 2025-06-20 01:39:05 UTC 1.121.164.79

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -61,3 +61,4 @@ VALUES ('alice123', 'alice@example.com','hashed123', 'owner'),
        ('adelle', 'adelle@gmail.com', 'Password123!', 'owner'),
        ('test', 'test@test.com', 'test123!', 'walker');
 
+INSERT INTO d
