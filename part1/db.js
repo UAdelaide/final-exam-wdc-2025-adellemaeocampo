4c23126d9f3ce0ed370023e02e4f8dfac9d79ad2
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 02:30:59 +0000

    [automatic commit] 2025-06-20 02:30:59 UTC 1.121.164.79

--- part1/db.js
+++ part1/db.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-//would put in .env but 
+//would put in .env but couldnt create one
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password:'',
   database: 'DogWalkService'
 });
+
 
 
 
