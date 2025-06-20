97b4782eecf04db0459702667bc11cc7e1f48692
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:54:02 +0000

    [automatic commit] 2025-06-20 11:54:02 UTC 1.121.164.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,6 +51,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    //for session implementation
     req.session.user = rows[0];
 
     res.json({ message: 'Login successful', user: rows[0] });
