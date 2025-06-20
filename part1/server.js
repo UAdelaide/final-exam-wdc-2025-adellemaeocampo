a6f108c7b183f96f547d9bea7e43a60c300344e6
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 03:45:31 +0000

    [automatic commit] 2025-06-20 03:45:30 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -35,7 +35,8 @@ app.get('/api/dogs', (req,res) => {
     const [dogs] = await db.query('SELECT * FROM Dogs');
     res.json(dogs);
   } catch (err) {
-    console.error('Error fetching dogs for route /api/dog')
+    console.error('Error fetching dogs for route /api/dogs:' err);
+    
   }
 
 
