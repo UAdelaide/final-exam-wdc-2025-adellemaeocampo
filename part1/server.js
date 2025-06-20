a57b9d10c5d6d4257bff9dfcec4a527c4693dcef
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 03:33:01 +0000

    [automatic commit] 2025-06-20 03:33:01 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -20,7 +20,7 @@ const PORT=8080;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO Users(unsername, email, password_hash, role)
+        INSERT INTO Users(username, email, password_hash, role)
         VALUES ('testUser', 'test@tester.com', 'tester123!', 'owner');
       `);
     }
