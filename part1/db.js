4b4ac8f5f06c20b782c6f98620a62a3f4f57a37e
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 04:02:13 +0000

    [automatic commit] 2025-06-20 04:02:13 UTC 1.121.164.79

--- part1/db.js
+++ part1/db.js
@@ -1,14 +1,12 @@
 
 const mysql = require('mysql2/promise');
 
-//would put in .env but couldnt create one
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password:'',
+  password: '',
   database: 'DogWalkService'
 });
 
 module.exports = db;
-
 
