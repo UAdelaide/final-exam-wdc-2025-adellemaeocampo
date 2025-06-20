f0d22374a9ceb0111f37c216f36f7654c355b805
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:50:46 +0000

    [automatic commit] 2025-06-20 11:50:46 UTC 1.121.164.79

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+//
 const session = require('express-session');
 
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-//
+//q13 added sessions
 app.use(session({
   secret: 'secretKey123',
   resave: false,
