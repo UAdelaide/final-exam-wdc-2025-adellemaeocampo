07752493d79337a71f94602646963512bf814055
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 03:54:16 +0000

    [automatic commit] 2025-06-20 03:54:16 UTC 1.121.164.79

--- part1/db.js
+++ part1/db.js
@@ -1,14 +1,14 @@
 
-const mysql = require('mysql2/promise');
+// const mysql = require('mysql2/promise');
 
-//would put in .env but couldnt create one
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password:'',
-  database: 'DogWalkService'
-});
+// //would put in .env but couldnt create one
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password:'',
+//   database: 'DogWalkService'
+// });
 
-module.exports = db;
+// module.exports = db;
 
 
