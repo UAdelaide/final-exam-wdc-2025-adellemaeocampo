97ce204b0a9fd3db74c24f34d1c43bad927660a8
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 06:07:42 +0000

    [automatic commit] 2025-06-20 06:07:42 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -42,7 +42,17 @@ app.get('/api/dogs', async (req,res) => {
 app.get('/api/walkrequests/open', async(req,res) => {
   try{
     const[openRequests] = await db.query (
-      ``
+      ` SELECT
+        wr.request_id,
+        d.name AS dog_name,
+        wr.requested_time,
+        wr.duration_minutes,
+        wr.location,
+        u.username AS owner_username
+      FROM WalkRequests wr
+      JOIN Dogs d ON wr.dog_id = d.dog_id
+      JOIN Users u ON d.owner_id = u.user_id
+      WHERE wr.status = 'open'`
     );
     res.json(openRequests);
   } catch (err) {
