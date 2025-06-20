06d6724d4dff20be9635785f782cf9d0ab50dbc8
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 01:46:02 +0000

    [automatic commit] 2025-06-20 01:46:02 UTC 1.121.164.79

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -64,4 +64,5 @@ VALUES ('alice123', 'alice@example.com','hashed123', 'owner'),
 INSERT INTO Dogs(owner_id, name, size)
 VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-       ((SELECT user_id FROM Users WHERE username = 'carol123'), '', 'large'),
+       ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Mochi', 'large'),
+       ((SELECT user_id FROM Users WHERE username = 'adelle'), 'Bella', 'small'),
