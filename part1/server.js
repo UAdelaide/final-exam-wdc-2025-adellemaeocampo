9107dc9f0dfe986bf8cf114873b5359e23a4ae70
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 06:14:57 +0000

    [automatic commit] 2025-06-20 06:14:57 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -65,9 +65,9 @@ app.get('/api/walkers/summary', async(req, res) => {
     const [walkerSummary] = await db.query (
       `SELECT
       u.username AS walker_username,
-      COUNT(DISTINCT r.request_id) AS completed_walks,
       COUNT(r.rating_id) AS total_ratings,
-      ROUND(AVG(r.rating), 1) AS average_rating
+      ROUND(AVG(r.rating), 1) AS average_rating,
+      COUNT(DISTINCT r.request_id) AS completed_walks
       FROM Users u
       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
       WHERE u.role = 'walker'
