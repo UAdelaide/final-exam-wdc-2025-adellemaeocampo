2dec0f896e3efcfa1bfe9629ee60fd4b7029cd3e
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:31:36 +0000

    [automatic commit] 2025-06-20 11:31:36 UTC 1.121.164.79

--- part2/app.js
+++ part2/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = require('express')
+const session = require('express-session');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
