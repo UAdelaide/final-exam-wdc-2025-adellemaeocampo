a7c999e4df04f70e4457b0b9f7001e2c3316cb79
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 04:15:52 +0000

    [automatic commit] 2025-06-20 04:15:52 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -46,8 +46,7 @@ app.get('/api/walkrequests/open', async(req,res) => {
     );
     res.json(openRequests);
   } catch (err) {
-    console.error('Error fetching walk requests for route /api/walkrequests/open:', err);
-    res.status(500).json({error: 'Failed to fetch walkrequest route'});
+    res.status(500).json({error: 'Failed to fetch walkrequests through api/walkrequests/open route'});
   }
 });
 
