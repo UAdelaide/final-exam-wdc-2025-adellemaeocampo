92a856f1847ea6230c171f58fa18ca87ec5a38ff
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 11:26:20 +0000

    [automatic commit] 2025-06-20 11:26:20 UTC 1.121.164.79

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,7 +187,9 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome " + data.user.user);
+            alert("Welcome " + data.user.username);
+
+            
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
