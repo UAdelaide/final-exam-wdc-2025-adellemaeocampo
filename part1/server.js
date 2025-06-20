3d5bf29f48e708b04229eed1181d3201062b6512
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 03:28:51 +0000

    [automatic commit] 2025-06-20 03:28:51 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -13,10 +13,13 @@ const mysql = require('mysql2');
 (async () => {
   try {
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        author VARCHAR(255)
+      CREATE TABLE IF NOT EXISTS Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('owner', 'walker') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
     `);
 
