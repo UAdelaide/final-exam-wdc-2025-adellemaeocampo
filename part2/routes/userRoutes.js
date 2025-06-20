13d391eb3be59bc629a7a93417808a644b55b5f0
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 14:01:54 +0000

    [automatic commit] 2025-06-20 14:01:54 UTC 1.121.164.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,11 @@ router.post('/login', async (req, res) => {
 
 //adding logout route
 router.post('/logout', (req, res) => {
-  req.session.destroy
+  req.session.destroy(err => {
+    if(err) {
+      return res.status(500).
+    }
+  })
 });
 
 module.exports = router;
