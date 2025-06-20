71e0ebc463da2bf86cfadcb7dd17b4cf8276a0ba
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 14:14:27 +0000

    [automatic commit] 2025-06-20 14:14:27 UTC 1.121.164.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,7 @@ router.post('/login', async (req, res) => {
 
 //adding logout route
 router.post('/logout', (req, res) => {
-  //destroys c
+  //destroys session form login
   req.session.destroy(err => {
     if(err) {
       return res.status(500).json({ error: 'failed to logout'});
