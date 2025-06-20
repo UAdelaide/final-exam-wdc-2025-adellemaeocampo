114e77cdd32cf589462b7faf26eaed50f8737e4e
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-20 03:58:04 +0000

    [automatic commit] 2025-06-20 03:58:04 UTC 1.121.164.79

--- part1/server.js
+++ part1/server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
@@ -9,7 +8,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 let db;
 const PORT=8080;
