08c06ec55e40de56988ee1001a42a0027678987b
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-21 01:30:36 +0000

    [automatic commit] 2025-06-21 01:30:36 UTC 1.121.164.79

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,3 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
+router.get('/dogs', async (req,res) => {
+  try {
+    const [dogs] = await db.query('SELECT * FROM Dogs');
+    res.json(dogs);
+  } catch (err) {
+    console.error('Error fetching dogs for route /api/dogs:', err);
+    res.status(500).json({error: 'Failed to fetch dogs through /api/dogs route'});
+  }
+});
+
+module.exports = router;
