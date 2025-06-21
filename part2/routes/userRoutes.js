72efeb73c150e11aa957e208cba2119a24d7d95a
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2025-06-21 01:26:02 +0000

    [automatic commit] 2025-06-21 01:26:02 UTC 1.121.164.79

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,4 +88,14 @@ router.post('/logout', (req, res) => {
   });
 });
 
+router.get('/api/dogs', async (req,res) => {
+  try {
+    const [dogs] = await db.query('SELECT * FROM Dogs');
+    res.json(dogs);
+  } catch (err) {
+    console.error('Error fetching dogs for route /api/dogs:', err);
+    res.status(500).json({error: 'Failed to fetch dogs through /api/dogs route'});
+  }
+});
+
 module.exports = router;
